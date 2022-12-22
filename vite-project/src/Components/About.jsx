defab0e1294c60dc800814a7f40adffd7e43c34e
Ron Alec <balidronalec@gmail.com>
2022-12-22 21:48:11 +0800

    final touch

--- vite-project/src/Components/About.jsx
+++ vite-project/src/Components/About.jsx
@@ -7,6 +7,8 @@ export default function About() {
       <p>I am a 4th year BSIT student who is invested in learning React
         and other web development technologies. I am mainly a frontend developer 
         but I can also work with backend development.
+        My capstone team also build a website called Horada.
+        I am their capstone manager.
       </p>
     </div>
   )
